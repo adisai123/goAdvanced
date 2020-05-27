75217845cc7fd294b1f68ea7b3d89b1a7bb729c8
Aditya Malpani <admalpan@cisco.com>
2020-05-27 16:10:47 +0530

    first

---
+++ src/race/race.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var Wait sync.WaitGroup
+var Counter int = 0
+
+func main() {
+
+	for routine := 1; routine <= 2; routine++ {
+
+		Wait.Add(1)
+		go Routine(routine)
+	}
+
+	Wait.Wait()
+	fmt.Printf("Final Counter: %d\n", Counter)
+}
+
+func Routine(id int) {
+
+	for count := 0; count < 2; count++ {
+
+		value := Counter
+		value++
+		Counter = value
+	}
+
+	Wait.Done()
+}
